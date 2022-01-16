0ccc7e76d50cd17a01ad34a8e0ab88c3e5af0c85
RizAli12 <rizwana.ali12@gmail.com>
2022-01-16 13:52:22 +0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,44 @@
 <script>
+  let html = "<p>Write some text!</p>";
+
+  let value = `Some words are *italic*, some are **bold**`;
+
+  let scoops = 1;
+  let flavours = ["Mint choc chip"];
+
+  let menu = ["Cookies and cream", "Mint choc chip", "Raspberry ripple"];
+
+  function join(flavours) {
+    if (flavours.length === 1) return flavours[0];
+    return `${flavours.slice(0, -1).join(", ")} and ${
+      flavours[flavours.length - 1]
+    }`;
+  }
+
+  let questions = [
+    { id: 1, text: `Where did you go to school?` },
+    { id: 2, text: `What is your mother's name?` },
+    {
+      id: 3,
+      text: `What is another personal fact that an attacker could easily find with Google?`,
+    },
+  ];
+
+  let selected;
+
+  let answer = "";
+
+  function handleSubmit() {
+    alert(
+      `answered question ${selected.id} (${selected.text}) with "${answer}"`
+    );
+  }
+
+  let yes = false;
+
+  let a = 1;
+  let b = 2;
+
   import CustomButton from "./CustomButton.svelte";
 
   function handleClick01() {
@@ -130,6 +170,92 @@
 
   <h1>Hello {name2.toUpperCase()}!</h1>
 
+  <h2>Insecurity questions</h2>
+
+  <form on:submit|preventDefault={handleSubmit}>
+    <select bind:value={selected} on:change={() => (answer = "")}>
+      {#each questions as question}
+        <option value={question}>
+          {question.text}
+        </option>
+      {/each}
+    </select>
+
+    <input bind:value={answer} />
+
+    <button disabled={!answer} type="submit"> Submit </button>
+  </form>
+
+  <p>selected question {selected ? selected.id : "[waiting...]"}</p>
+
+  <label>
+    <input type="number" bind:value={a} min="0" max="10" />
+    <input type="range" bind:value={a} min="0" max="10" />
+  </label>
+
+  <label>
+    <input type="number" bind:value={b} min="0" max="10" />
+    <input type="range" bind:value={b} min="0" max="10" />
+  </label>
+
+  <p>{a} + {b} = {a + b}</p>
+
+  <h3>Select your ICECREAM</h3>
+
+  <h2>Size</h2>
+
+  <label>
+    <input type="radio" bind:group={scoops} value={1} />
+    One scoop
+  </label>
+
+  <label>
+    <input type="radio" bind:group={scoops} value={2} />
+    Two scoops
+  </label>
+
+  <label>
+    <input type="radio" bind:group={scoops} value={3} />
+    Three scoops
+  </label>
+
+  <h2>Flavours</h2>
+
+  <select multiple bind:value={flavours}>
+    {#each menu as flavour}
+      <option value={flavour}>
+        {flavour}
+      </option>
+    {/each}
+  </select>
+
+  {#if flavours.length === 0}
+    <p>Please select at least one flavour</p>
+  {:else if flavours.length > scoops}
+    <p>Can't order more flavours than scoops!</p>
+  {:else}
+    <p>
+      You ordered {scoops}
+      {scoops === 1 ? "scoop" : "scoops"}
+      of {join(flavours)}
+    </p>
+  {/if}
+
+  <label>
+    <input type="checkbox" bind:checked={yes} />
+    Yes! Send me regular email spam
+  </label>
+
+  {#if yes}
+    <p>Thank you. We will bombard your inbox and sell your personal details.</p>
+  {:else}
+    <p>
+      You must opt in to continue. If you're not paying, you're the product.
+    </p>
+  {/if}
+
+  <button disabled={!yes}> Subscribe </button>
+
   <CustomButton on:click={handleClick01} />
 
   <p>
@@ -175,6 +301,10 @@
     The mouse position is {m.x} x {m.y}
   </div>
 
+  <div contenteditable="true" bind:innerHTML={html} />
+
+  <pre>{html}</pre>
+
   <h2>The Famous Cats of YouTube</h2>
 
   <ul>
@@ -203,6 +333,11 @@
     background-color: #c9c8c1;
   }
 
+  h3 {
+    font-size: 3em;
+    background-color: blueviolet;
+  }
+
   h1 {
     color: #ff3e00;
     text-transform: uppercase;
@@ -214,6 +349,12 @@
     text-align: left;
     color: rgb(113, 140, 212);
   }
+
+  textarea {
+    width: 100%;
+    height: 200px;
+  }
+
   ul {
     text-align: left;
     border-radius: 0.2em;
@@ -229,11 +370,28 @@
     main {
       max-width: none;
     }
+  }
+  div {
+    background-color: #e75626;
+    width: 100%;
+    height: 100%;
+  }
 
-    div {
-      background-color: #e75626;
-      width: 100%;
-      height: 100%;
-    }
+  label {
+    display: flex;
+    background-color: rgb(182, 226, 226);
+  }
+  input {
+    margin: 6px;
+  }
+
+  input {
+    max-width: 100%;
+  }
+
+  [contenteditable] {
+    padding: 0.5em;
+    border: 1px solid #eee;
+    border-radius: 4px;
   }
 </style>
